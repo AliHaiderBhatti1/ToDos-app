f9ed00d9b7282752d7b205b25c42dd527436eb0e
AliHaiderBhatti1 <ali.haider6080@gmail.com>
2021-11-21 03:05:16 +0500

    add email, password validations

---
+++ src/validations/validations.js
@@ -0,0 +1,12 @@
+const emailRules = [
+    (v) => !!v || "Email is required",
+    (v) =>
+      /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(
+        v
+      ) || "Email must be valid",
+  ];
+  
+  const required = [(v) => !!v || "Required."];
+  
+  export {  emailRules, required };
+  
